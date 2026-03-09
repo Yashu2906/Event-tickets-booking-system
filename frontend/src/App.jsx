653337da3f1eff2784062307ffe82e7d386489fd
Yash Patel <patelyashu001@gmail.com>
2026-03-09 12:17:11 +0530

    Routing setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+import MyBookings from "./pages/MyBookings";
+import EventDetails from "./pages/EventDetails";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/bookings" element={<MyBookings />} />
+        <Route path="/events/:id" element={<EventDetails />} />
       </Routes>
     </BrowserRouter>
   );
